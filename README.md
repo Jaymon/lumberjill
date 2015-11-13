47a1d59ab12d4e1a1425079d9cf61e5496d3358a
Topher <topher@firstopinionapp.com>
2015-11-12 16:34:49 -0800

    Setup new repo.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lumberjack
+
+This library provides additional logging filters and handlers for use with the
+Python logging framework.
+
+Lovingly crafted for [First Opinion](http://firstopinionapp.com).
+
+## Installation
+
+    $ pip install lumberjack
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# I shamefully ripped most of this off from fbconsole
+# http://docs.python.org/distutils/setupscript.html
+# http://docs.python.org/2/distutils/examples.html
+
+import sys
+from setuptools import setup
+import ast
+import os
+
+name = 'lumberjack'
+version = ''
+with open('{}{}__init__.py'.format(name, os.sep), 'rU') as f:
+    for node in (n for n in ast.parse(f.read()).body if isinstance(n, ast.Assign)):
+        node_name = node.targets[0]
+        if isinstance(node_name, ast.Name) and node_name.id.startswith('__version__'):
+            version = node.value.s
+            break
+
+if not version:
+    raise RuntimeError('Unable to find version number')
+
+setup(
+    name=name,
+    version=version,
+    description='Provides additional logging filters and handlers.',
+    author='Jay Marcyes',
+    author_email='jay@marcyes.com',
+    url='http://github.com/firstopinion/{}'.format(name),
+    packages=[name, '{}.interface'.format(name)],
+    tests_require=[],
+    license="MIT",
+    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+    ],
+    #test_suite = "{}_test".format(name),
+)
