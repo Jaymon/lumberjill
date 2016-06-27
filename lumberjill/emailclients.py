27680972377595190648045dddeb544db9cc38ad
Topher <topher@firstopinionapp.com>
2016-06-27 15:07:03 -0700

    Add Mandrill email client.

--- lumberjill/emailclients.py
+++ lumberjill/emailclients.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import mandrill
 import sendgrid
 
 # Prevent infinite logging loops. If the user wants the log messages, then
@@ -8,6 +9,40 @@ import sendgrid
 logger = logging.getLogger(__name__)
 logger.propagate = False
 logger.addHandler(logging.NullHandler())
+
+
+class MandrillEmail(object):
+    """
+    This implementation uses Mandrill to send email.
+    """
+    def __init__(self, api_key):
+        """
+        Initialize email client.
+
+        param api_key: Mandrill API Key
+        """
+        self._client = mandrill.Mandrill(api_key)
+
+    def send(self, mime_email):
+        """
+        Sends the provided email. Checks the 'NOTIFY_EMAIL' environment
+        variable to decide whether to actually send the email or to log it.
+
+        :param mime_email: MIMEType email to be sent via Mandrill
+        """
+        message = {
+            'from_email': mime_email['From'],
+            'from_name': mime_email.from_name,
+            'subject': mime_email['Subject'],
+            'text': mime_email.get_payload(),
+            'to': [{'email': mime_email['To'], 'type': 'to', 'name': ""}],
+        }
+
+        if bool(int(os.environ.get('NOTIFY_EMAIL', 0))):
+            results = self._client.messages.send(message=message, async=False)
+            logger.info("Results of sending email [{}]: {}".format(message['subject'], results))
+        else:
+            logger.info("NOTIFY_EMAIL is off. Didn't send this: " + _email_headers(mime_email))
 
 
 class SendGridEmail(object):
